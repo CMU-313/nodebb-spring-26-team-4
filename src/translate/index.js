abe63c8b63e61f62a31351d1ae0e24f2dafe92e5
Gorden Jin <yijin2@andrew.cmu.edu>
2026-04-03 00:19:00 +0000

    fix: microservice connection API

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,14 +4,25 @@
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
-};
-
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
+// translatorApi.translate = function (postData) {
+//     return ['is_english',postData];
 // };
+
+translatorApi.translate = async function (postData) {
+//  Edit the translator URL below
+	const TRANSLATOR_API = 'http://host.docker.internal:5000';
+	try{
+		// console.log('fetching from');
+		// console.log(TRANSLATOR_API + '/?content=' + postData.content);
+		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+		const data = await response.json();
+		// console.log('Success');
+		// console.log(data.is_english);
+		// console.log(data.translated_content);
+		// console.log('content output above');
+		return [data.is_english, data.translated_content];
+	} catch (e) {
+		console.log('Not good');
+		return ['is_english', postData];
+	}
+};
