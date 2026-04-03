72500cdba7983bc3d24829a19854a85b08b6544c
alexbock2 <alexandrabock14@gmail.com>
2026-04-03 23:35:01 +0000

    translate with vm link

--- src/translate/index.js
+++ src/translate/index.js
@@ -10,19 +10,12 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 //  Edit the translator URL below
-	const TRANSLATOR_API = 'http://host.docker.internal:5000';
+	const TRANSLATOR_API = 'http://17313-team06.s3d.cmu.edu:5000';
 	try{
-		// console.log('fetching from');
-		// console.log(TRANSLATOR_API + '/?content=' + postData.content);
 		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
 		const data = await response.json();
-		// console.log('Success');
-		// console.log(data.is_english);
-		// console.log(data.translated_content);
-		// console.log('content output above');
 		return [data.is_english, data.translated_content];
 	} catch (e) {
-		console.log('Not good');
 		return ['is_english', postData];
 	}
 };
